dae4ab4062f5d32e6ec18ec344ae6cd642744e8e
Tharaka234 <tharakaisuru313@gmail.com>
2026-08-25 11:41:31 +0530

    I updated transaction.dart

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -12,4 +12,26 @@ class Transaction {
     required this.date,
     required this.category,
   });
+
+  // Convert a Transaction into a Map for sqflite.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String(),
+      'category': category,
+    };
+  }
+
+  // Build a Transaction from a database row.
+  factory Transaction.fromMap(Map<String, dynamic> map) {
+    return Transaction(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      amount: (map['amount'] as num).toDouble(),
+      date: DateTime.parse(map['date'] as String),
+      category: map['category'] as String,
+    );
+  }
 }
